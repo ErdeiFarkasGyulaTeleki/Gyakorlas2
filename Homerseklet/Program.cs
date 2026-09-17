2d689d897d492595d902fe588c3e82ef026c0287
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-17 08:27:16 +0200

    Atvalto hasznalata

--- Homerseklet/Program.cs
+++ Homerseklet/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using Homerseklet;
+
+HomersekletAtvalto atvalto = new HomersekletAtvalto(25);
+
+Console.WriteLine($"25 C°: {atvalto.ToFahrenheit()} °F");
+Console.WriteLine($"38 C°: {HomersekletAtvalto.CelsiusToFahrenheit(38)} °F");
