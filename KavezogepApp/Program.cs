30a3d1d3fc1c6e9e24df38f4a80258324e0fccea
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-17 08:37:30 +0200

    Kavezogep felhasznalasa a Program-ban

--- KavezogepApp/Program.cs
+++ KavezogepApp/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using KavezogepApp;
+
+Kavezogep kavezogep = new Kavezogep(100);
+Console.WriteLine(kavezogep.ToString());
+
+kavezogep.KaveFozes();
+
+kavezogep.VizTolt(200);
+Console.WriteLine(kavezogep.ToString());
+
+kavezogep.KaveFozes();
+kavezogep.KaveFozes();
+
+Console.WriteLine(kavezogep.ToString());
+
+kavezogep.KaveFozes();
+Console.WriteLine(kavezogep.ToString());
