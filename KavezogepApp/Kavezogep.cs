e82321f611fa55ff80979f2058df8f4ca886498b
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-17 08:35:06 +0200

    Kavezogep megirasa

--- KavezogepApp/Kavezogep.cs
+++ KavezogepApp/Kavezogep.cs
@@ -1,10 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace KavezogepApp
+﻿namespace KavezogepApp
 {
-    internal class Kavezogep
+    public class Kavezogep
     {
+        private int vizKeszletMl;
+
+        public Kavezogep(int _vizKeszletMl)
+        {
+            vizKeszletMl = _vizKeszletMl;
+        }
+
+        public void VizTolt(int ml)
+        {
+            if (ml < 1)
+            {
+                Console.WriteLine("A töltött víz mennyisége csak pozitív lehet!");
+                return;
+            }
+
+            vizKeszletMl += ml;
+
+            Console.WriteLine($"Sikeresen töltött {ml} ml vizet.");
+        }   
+
+        public void KaveFozes()
+        {
+            if (vizKeszletMl < 150)
+            {
+                Console.WriteLine("Nem lehet 150 ml-nél kevesebb vízből kávét főzni!");
+                return;
+            }
+
+            vizKeszletMl -= 150;
+
+            Console.WriteLine("Sikeres kávéfőzés!");
+        }
+
+        public override string ToString()
+        {
+            return $"A kávégépben {vizKeszletMl} ml víz van.";
+        }
     }
 }
