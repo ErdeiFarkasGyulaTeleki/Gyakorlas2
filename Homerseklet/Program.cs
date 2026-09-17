28a0e8a011c78191fd5117c1a214c5a6a4dbfe7b
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-17 08:27:38 +0200

    helyesirasi hiba javitasa

--- Homerseklet/Program.cs
+++ Homerseklet/Program.cs
@@ -2,5 +2,5 @@
 
 HomersekletAtvalto atvalto = new HomersekletAtvalto(25);
 
-Console.WriteLine($"25 C°: {atvalto.ToFahrenheit()} °F");
-Console.WriteLine($"38 C°: {HomersekletAtvalto.CelsiusToFahrenheit(38)} °F");
+Console.WriteLine($"25 °C: {atvalto.ToFahrenheit()} °F");
+Console.WriteLine($"38 °C: {HomersekletAtvalto.CelsiusToFahrenheit(38)} °F");
