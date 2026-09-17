c9545f60b217f70a67c9215c95395de41a641b45
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-17 08:23:11 +0200

    Atvalto megirasa

--- Homerseklet/HomersekletAtvalto.cs
+++ Homerseklet/HomersekletAtvalto.cs
@@ -2,5 +2,21 @@
 {
     public class HomersekletAtvalto
     {
+        public double Celsius;
+
+        public HomersekletAtvalto(double _celsius)
+        {
+            Celsius = _celsius;
+        }
+
+        public double ToFahrenheit()
+        {
+            return Celsius * 9 / 5 + 32;
+        }
+
+        public static double CelsiusToFahrenheit(double _celsius)
+        {
+            return _celsius * 9 / 5 + 32;
+        }
     }
 }
