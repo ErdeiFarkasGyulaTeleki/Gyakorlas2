937740f0f6b8ad6a37ee9453eea1c0d242b5ad7d
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-17 08:45:56 +0200

    Okostelefon hasznalata a Program-ban

--- TelefonApp/Program.cs
+++ TelefonApp/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using TelefonApp;
+
+Okostelefon telefon1 = new Okostelefon("Samsung Galaxy S21", 75);
+Okostelefon telefon2 = new Okostelefon("iPhone 13", 50);
+
+Console.WriteLine(telefon1.ToString());
+
+telefon1.Toltottseg = -24;
+Console.WriteLine(telefon1.ToString());
+
+telefon1.Toltottseg = 134;
+Console.WriteLine(telefon1.ToString());
+
+telefon1.Toltottseg = 68;
+Console.WriteLine(telefon1.ToString());
