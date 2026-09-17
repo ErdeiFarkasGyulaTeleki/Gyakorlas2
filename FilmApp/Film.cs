d2937b38575fec9ecb153357832acd3d66137bb2
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-17 08:14:57 +0200

    Film osztaly letrehozasa

--- FilmApp/Film.cs
+++ FilmApp/Film.cs
@@ -6,5 +6,18 @@ namespace FilmApp
 {
     internal class Film
     {
+        public string Cim;
+        public int HosszPercben;
+
+        public Film(string _cim, int _hosszPercben)
+        {
+            Cim = _cim;
+            HosszPercben = _hosszPercben;
+        }
+
+        public override string ToString()
+        {
+            return $"A {Cim} című film {HosszPercben} perces)";
+        }
     }
 }
